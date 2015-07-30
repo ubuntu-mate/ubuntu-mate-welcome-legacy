6266cbb023bf880aa7d08160310ddba517575f09
Luke Horwell <lukehorwell37+git@gmail.com>
2015-07-30 20:36:16 +0100

    "Back To Top" appears for long pages.

--- data/js/welcome.js
+++ data/js/welcome.js
@@ -1,3 +1,23 @@
 function WelcomeCtrl($scope) {
 
 }
+
+// Scroll to the top
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+});
