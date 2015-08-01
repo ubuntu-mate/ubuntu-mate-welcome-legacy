bbd1cdd515df392fc3026736d62d844ea7ba47e2
Luke Horwell <lukehorwell37+git@gmail.com>
2015-08-01 19:23:10 +0100

    Add categories and animation to software page.

--- data/js/welcome.js
+++ data/js/welcome.js
@@ -32,3 +32,40 @@ $(document).ready(function () {
     });
 
 });
+
+
+// Software Page - Categories for Apps
+
+    // Set the first landing category and animations.
+    var currentCategory;
+
+    // Show the first category.
+    currentCategory = '#MoreApps2';
+    $(currentCategory).jAnimateOnce('zoomInRight');
+    $(currentCategory).removeClass('hideSection');
+
+    // Switch to another category.
+    function switchCategory(now, next) {
+        $(now).jAnimateOnce('fadeOut', function(self, effect){
+            // Finished exit animation
+            $(now).addClass('hideSection');
+
+            // Show next category
+            currentCategory = next;
+            $(next).removeClass('hideSection');
+            $(next).jAnimateOnce('fadeInLeft');
+        });
+        return currentCategory;
+    }
+
+    // Button triggers for functions
+    $("#Accessories1").click(function(){ switchCategory(currentCategory, '#Accessories2');  });
+    $("#Games1").click(function(){       switchCategory(currentCategory, '#Games2');  });
+    $("#Graphics1").click(function(){    switchCategory(currentCategory, '#Graphics2');  });
+    $("#Internet1").click(function(){    switchCategory(currentCategory, '#Internet2');  });
+    $("#Office1").click(function(){      switchCategory(currentCategory, '#Office2');  });
+    $("#Programming1").click(function(){ switchCategory(currentCategory, '#Programming2');  });
+    $("#Media1").click(function(){       switchCategory(currentCategory, '#Media2');  });
+    $("#SysTools1").click(function(){    switchCategory(currentCategory, '#SysTools2');  });
+    $("#UnivAccess1").click(function(){  switchCategory(currentCategory, '#UnivAccess2');  });
+    $("#MoreApps1").click(function(){    switchCategory(currentCategory, '#MoreApps2');  });
