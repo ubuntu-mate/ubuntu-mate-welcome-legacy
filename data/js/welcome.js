6ff0de5409942a4068f86ac600be8ab31d99b621
Luke Horwell <luke@ubuntu-mate.org>
2016-05-29 14:56:54 +0100

    Multiple improvements to Boutique Search.
    * Now checks descriptions and "alternate to" in addition to name.
    * Abolishes regular expressions for Python's built-in functions.
    * Respects non-free toggle.

--- data/js/welcome.js
+++ data/js/welcome.js
@@ -542,6 +542,12 @@ if ( current_page == 'software.html' ) {
       keywords = $('#search-terms').val();
       cmd('search?' + keywords)
     }
+
+    // Search again but include non-free matches.
+    function searchAgainNonFree() {
+      toggleNonFree()
+      searchNow()
+    }
 }
 
 
