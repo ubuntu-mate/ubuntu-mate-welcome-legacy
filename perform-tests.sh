60ca9002d93b6e784d2822d2f2056aa0d64c4cc8
Luke Horwell <luke@ubuntu-mate.org>
2017-04-13 13:21:27 +0100

    Add parameters to local test script.

--- perform-tests.sh
+++ perform-tests.sh
@@ -2,21 +2,41 @@
 #
 # Test ubuntu-mate-welcome locally.
 #
+# These parameters specify the length of the test:
+# --quick
+# --extended
+#
 
 repo_root=$(dirname "$0")
 success=true
-
-echo -e "\n-------- Start of Testing --------"
 cd "$repo_root"
 
-for test in "$repo_root"/tests/*.py
-do
-    python3 "$test"
+# Determine which tests to run
+tests=""
+if [ "$1" == "--quick" ]; then
+    tests="all-json-check.py validate-apps.py image-extensions.py check-trans-code-tags.py check-trans-page-templates.py"
+
+elif [ "$1" == "--extended" ]; then
+    for test in "$repo_root"/tests/*.py; do
+        tests+=" $(basename $test)"
+    done
+
+else
+    echo "Please specify one of the following:"
+    echo " "
+    echo " --quick      Checks for common errors and faults."
+    echo " --extended   Deep test, checks remote repositories."
+    exit 1
+fi
+
+# Perform each test
+echo -e "\n-------- Start of Testing --------"
+for test in $tests; do
+    python3 "$repo_root"/tests/"$test"
     if [ ! "$?" == "0" ]; then
         success=false
     fi
 done
-
 echo -e "\n-------- End of Testing  --------\n"
 
 if [ "$success" == true ]; then
