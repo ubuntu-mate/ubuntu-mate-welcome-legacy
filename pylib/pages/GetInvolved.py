f607522b8783a1ec6b2957b92dd05280eba81e2e
Luke Horwell <luke@ubuntu-mate.org>
2017-12-22 17:55:06 +0000

    Add new page modules.

---
+++ pylib/pages/GetInvolved.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+#
+# Ubuntu MATE Welcome is licensed under the GPLv3.
+#
+# Copyright (C) 2015-2017 Luke Horwell <luke@ubuntu-mate.org>
+#               2015-2016 Martin Wimpress <code@flexion.org>
+#
+
+"""
+Welcome Page: Get Involved
+
+Provides details on how to contribute back to the operating system.
+"""
+
+from .. import Common
+from time import sleep
+import os
+
+_ = Common.setup_translations(__file__, "ubuntu-mate-welcome")
+
+
+def page_enter(variables):
+    """
+    Triggered upon opening the page.
+    """
+    update_page = variables["objects"]["update_page"]
+    change_page = variables["objects"]["change_page"]
+    ui = variables["objects"]["ui"]
+
+
+def page_exit(variables):
+    """
+    Triggered upon closing the page.
+    """
+    update_page = variables["objects"]["update_page"]
+    pass
+
+
+def do_command(variables, cmd):
+    """
+    When a command is triggered from the page.
+    Returns True if matches here.
+    """
+    parameters = cmd.split('?')[1:]
+    return False
---
+++ pylib/pages/MainMenu.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+#
+# Ubuntu MATE Welcome is licensed under the GPLv2.
+#
+# Copyright (C) 2017 Luke Horwell <luke@ubuntu-mate.org>
+#
+
+"""
+Welcome Page: Main Menu
+
+The main menu for the application.
+"""
+
+from .. import Common
+from time import sleep
+import os
+import sys
+
+_ = Common.setup_translations(__file__, "ubuntu-mate-welcome")
+
+
+def page_enter(variables):
+    """
+    Triggered upon opening the page.
+    """
+    update_page = variables["objects"]["update_page"]
+    change_page = variables["objects"]["change_page"]
+    ui = variables["objects"]["ui"]
+
+    update_page("header", "show")
+    update_page("footer", "show")
+    update_page("main", "show")
+
+    update_page("content", "append", ui.print_button("Normal", "quit", None, "normal"))
+
+
+    #~ update_page("body", "append", "<script>cmd('test')</script>")
+
+
+def page_exit(variables):
+    """
+    Triggered upon closing the page.
+    """
+    update_page = variables["objects"]["update_page"]
+
+    print("Exit")
+    pass
+
+
+def do_command(variables, cmd):
+    """
+    When a command is triggered from the page.
+    Returns True if matches here.
+    """
+    parameters = cmd.split('?')[1:]
+
+    #~ if cmd == "test":
+        #~ return True
