644224170e8358f1066ed14c7b53e970874f67f9
Luke Horwell <luke@ubuntu-mate.org>
2018-01-13 16:55:42 +0000

    Migrate Shop page.

--- pylib/pages/Shop.py
+++ pylib/pages/Shop.py
@@ -24,8 +24,23 @@ def page_enter(variables):
     Triggered upon opening the page.
     """
     update_page = variables["objects"]["update_page"]
-    change_page = variables["objects"]["change_page"]
     ui = variables["objects"]["ui"]
+
+    content = ''
+    content += ui.print_title(_("Buy from the Boutique"))
+
+    content += '<img src="img/shop/laptop.png">' \
+                '<img src="img/shop/badge.png"/>' \
+                '<img src="img/shop/flash-drive.png"/>' \
+                '<img src="img/shop/mug.png"/>' \
+                '<img src="img/shop/shirt.png"/>' \
+
+    content += ui.print_paragraph(_("The Ubuntu MATE Boutique sells everything from computers " \
+                "that come pre-installed with Ubuntu MATE to clothing and " \
+                "apparel that are all Ubuntu MATE branded. Every item sold pays " \
+                "a small commission to the Ubuntu MATE project."))
+
+    update_page("content", "html", content)
     Common.page_enter_global(variables, _("Shop"))
 
 
