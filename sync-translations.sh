2cad3d15544824d9ee5d8c680c48c8ef59fa7cdb
Luke Horwell <code@horwell.me>
2020-03-09 12:44:40 +0000

    Add 'sync-translations' script.

---
+++ sync-translations.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xe
+#
+# Can only be run by the Ubuntu MATE team with access to Transifex.
+#
+tx pull -a --minimum-perc=5
+./welcome-po.py --update-pos
+./edgar-allan create-all-pots
+./edgar-allan translate-all
+tx push -s
