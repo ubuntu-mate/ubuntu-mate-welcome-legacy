8b070aaff28a1cc287d8b59bdd37e66084050a66
Luke Horwell <code@horwell.me>
2020-03-30 16:39:58 +0100

    Add regenerate-pots.sh script when syncing translations.

--- sync-translations.sh
+++ sync-translations.sh
@@ -3,6 +3,7 @@
 # Can only be run by the Ubuntu MATE team with access to Transifex.
 #
 tx pull -a --minimum-perc=5
+./regenerate-pots.sh
 ./welcome-po.py --update-pos
 ./edgar-allan create-all-pots
 ./edgar-allan translate-all
