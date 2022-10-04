49edd9d199c7fbc1a93b369bba4e4ac9cb16f613
franksmcb <franksmbc@gmail.com>
2022-10-04 13:34:47 -0600

    Initial stocking for 22.10
    Stock for 22.10

--- tests/validate-apps.py
+++ tests/validate-apps.py
@@ -14,7 +14,7 @@ import json
 ###############################################
 test.start()
 
-valid_distro_codenames = ['trusty', 'xenial', 'artful', 'bionic', 'disco', 'eoan', 'focal', 'groovy', 'hirsute', 'impish', 'jammy']
+valid_distro_codenames = ['trusty', 'xenial', 'artful', 'bionic', 'disco', 'eoan', 'focal', 'groovy', 'hirsute', 'impish', 'jammy', 'kinetic']
 valid_arch = ['i386', 'amd64', 'armhf', 'arm64']
 
 # Load Applications JSON
